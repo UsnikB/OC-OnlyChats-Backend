ec43b418c836e32272ee21cf5990fceef73da0d4
UsnikB <usnikbiswas@gmail.com>
2023-02-04 00:54:47 +0000

    added jwt, starting database setup

--- app.py
+++ app.py
@@ -3,15 +3,17 @@ from routes.routes import routes_bp, authentication_bp
 from flask_jwt_extended import JWTManager
 from config import Config
 
+# Created the Flask app
 app = Flask(__name__)
+
+# Registering route Blueprint
 app.register_blueprint(routes_bp)
 app.register_blueprint(authentication_bp)
 
-jwt = JWTManager(app)
-
 # Setup the Flask-JWT-Extended extension
-app.config["JWT_SECRET_KEY"] = Config.SECRET_KEY  # Change this!
+app.config["JWT_SECRET_KEY"] = Config.SECRET_KEY
 jwt = JWTManager(app)
 app.config["JWT_ALGORITHM"] = Config.JWT_ALGORITHM
+
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
